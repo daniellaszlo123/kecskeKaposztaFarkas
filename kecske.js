886a9f4ab6518001ef5dbfdbf57463a0ab853eb5
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-29 09:28:24 +0200

    3. feladat

---
+++ kecske.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", init);
+
+function ID(elem) {
+    return document.getElementById(elem);
+}
+
+var kepek = [];
+
+var kep1=ID(1);
+var kep2=ID(2);
+var kep3=ID(3);
+
+function init() {
+    ID(1).addEventListener("click", feltolt);
+    ID(2).addEventListener("click", feltolt);
+    ID(3).addEventListener("click", feltolt);
+    ID(1).addEventListener("click", kepStilus(kep1));
+    ID(2).addEventListener("click", kepStilus(kep2));
+    ID(3).addEventListener("click", kepStilus(kep3));
+}
+
+function feltolt() {
+    kepek.push(this.src);
+    console.log(kepek);
+}
+
+function kepStilus(kep) {
+    kep.style.display="none"
+}
